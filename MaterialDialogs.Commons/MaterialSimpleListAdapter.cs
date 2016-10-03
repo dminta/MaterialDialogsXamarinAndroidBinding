fb891fbdf74fef88aa4ae594815fbf14c95d706a
dminta <dominik@minta.it>
2016-10-04 00:40:59 +0200

    Initial commit

---
+++ MaterialDialogs.Commons/MaterialSimpleListAdapter.cs
@@ -0,0 +1,57 @@
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using System;
+
+namespace Com.Afollestad.Materialdialogs.Simplelist
+{
+    public partial class MaterialSimpleListAdapter : global::Android.Support.V7.Widget.RecyclerView.Adapter, global::Com.Afollestad.Materialdialogs.Internal.IMDAdapter
+    {
+        static IntPtr id_onBindViewHolder_Lcom_afollestad_materialdialogs_simplelist_MaterialSimpleListAdapter_SimpleListVH_I;
+        // Metadata.xml XPath method reference: path="/api/package[@name='com.afollestad.materialdialogs.simplelist']/class[@name='MaterialSimpleListAdapter']/method[@name='onBindViewHolder' and count(parameter)=2 and parameter[1][@type='com.afollestad.materialdialogs.simplelist.MaterialSimpleListAdapter.SimpleListVH'] and parameter[2][@type='int']]"
+        [Register("onBindViewHolder", "(Lcom/afollestad/materialdialogs/simplelist/MaterialSimpleListAdapter$SimpleListVH;I)V", "GetOnBindViewHolder_Lcom_afollestad_materialdialogs_simplelist_MaterialSimpleListAdapter_SimpleListVH_IHandler")]
+        public override unsafe void OnBindViewHolder(RecyclerView.ViewHolder p0, int p1)
+        {
+            if (id_onBindViewHolder_Lcom_afollestad_materialdialogs_simplelist_MaterialSimpleListAdapter_SimpleListVH_I == IntPtr.Zero)
+                id_onBindViewHolder_Lcom_afollestad_materialdialogs_simplelist_MaterialSimpleListAdapter_SimpleListVH_I = JNIEnv.GetMethodID(class_ref, "onBindViewHolder", "(Lcom/afollestad/materialdialogs/simplelist/MaterialSimpleListAdapter$SimpleListVH;I)V");
+            try
+            {
+                JValue* __args = stackalloc JValue[2];
+                __args[0] = new JValue(p0);
+                __args[1] = new JValue(p1);
+
+                if (GetType() == ThresholdType)
+                    JNIEnv.CallVoidMethod(((global::Java.Lang.Object)this).Handle, id_onBindViewHolder_Lcom_afollestad_materialdialogs_simplelist_MaterialSimpleListAdapter_SimpleListVH_I, __args);
+                else
+                    JNIEnv.CallNonvirtualVoidMethod(((global::Java.Lang.Object)this).Handle, ThresholdClass, JNIEnv.GetMethodID(ThresholdClass, "onBindViewHolder", "(Lcom/afollestad/materialdialogs/simplelist/MaterialSimpleListAdapter$SimpleListVH;I)V"), __args);
+            }
+            finally
+            {
+            }
+        }
+
+        static IntPtr id_onCreateViewHolder_Landroid_view_ViewGroup_I;
+        // Metadata.xml XPath method reference: path="/api/package[@name='com.afollestad.materialdialogs.simplelist']/class[@name='MaterialSimpleListAdapter']/method[@name='onCreateViewHolder' and count(parameter)=2 and parameter[1][@type='android.view.ViewGroup'] and parameter[2][@type='int']]"
+        [Register("onCreateViewHolder", "(Landroid/view/ViewGroup;I)Lcom/afollestad/materialdialogs/simplelist/MaterialSimpleListAdapter$SimpleListVH;", "GetOnCreateViewHolder_Landroid_view_ViewGroup_IHandler")]
+        public override unsafe RecyclerView.ViewHolder OnCreateViewHolder(global::Android.Views.ViewGroup p0, int p1)
+        {
+            if (id_onCreateViewHolder_Landroid_view_ViewGroup_I == IntPtr.Zero)
+                id_onCreateViewHolder_Landroid_view_ViewGroup_I = JNIEnv.GetMethodID(class_ref, "onCreateViewHolder", "(Landroid/view/ViewGroup;I)Lcom/afollestad/materialdialogs/simplelist/MaterialSimpleListAdapter$SimpleListVH;");
+            try
+            {
+                JValue* __args = stackalloc JValue[2];
+                __args[0] = new JValue(p0);
+                __args[1] = new JValue(p1);
+
+                global::Com.Afollestad.Materialdialogs.Simplelist.MaterialSimpleListAdapter.SimpleListVH __ret;
+                if (GetType() == ThresholdType)
+                    __ret = global::Java.Lang.Object.GetObject<global::Com.Afollestad.Materialdialogs.Simplelist.MaterialSimpleListAdapter.SimpleListVH>(JNIEnv.CallObjectMethod(((global::Java.Lang.Object)this).Handle, id_onCreateViewHolder_Landroid_view_ViewGroup_I, __args), JniHandleOwnership.TransferLocalRef);
+                else
+                    __ret = global::Java.Lang.Object.GetObject<global::Com.Afollestad.Materialdialogs.Simplelist.MaterialSimpleListAdapter.SimpleListVH>(JNIEnv.CallNonvirtualObjectMethod(((global::Java.Lang.Object)this).Handle, ThresholdClass, JNIEnv.GetMethodID(ThresholdClass, "onCreateViewHolder", "(Landroid/view/ViewGroup;I)Lcom/afollestad/materialdialogs/simplelist/MaterialSimpleListAdapter$SimpleListVH;"), __args), JniHandleOwnership.TransferLocalRef);
+                return __ret as RecyclerView.ViewHolder;
+            }
+            finally
+            {
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Material Dialogs binding for Xamarin Android
+
+Work in progress - compiling but not tested yet; sample app will be available soon
+
+Material Dialogs library v0.9.0.2 included
+
+Prerequisites:
+* http://motzcod.es/post/150380059392/preparing-machines-for-xamarin-cycle
+* Visual Studio 2015 with Xamarin.Android 7.0.1.2 or newer
