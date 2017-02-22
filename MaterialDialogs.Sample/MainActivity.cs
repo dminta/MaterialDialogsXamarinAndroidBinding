ce63156b53a40dacc5a1bf178b2a98f4dac598dd
dminta <dominik@minta.it>
2017-02-22 01:58:34 +0100

    Sample project stub with resources

---
+++ MaterialDialogs.Sample/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace MaterialDialogs.Sample
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView (Resource.Layout.activity_main);
+        }
+    }
+}
+
